66f52b509f7754c3ee9789dbbc52cf2152c2208a
Valera6 <v79166789533@gmail.com>
2023-11-07 16:20:52 +0000

    .

--- valera/src/requests/client.rs
+++ valera/src/requests/client.rs
@@ -1,4 +1,3 @@
-use crate::types::*;
 use anyhow::{self, Result};
 use chrono::{NaiveDateTime, Utc};
 use reqwest::Response;
@@ -75,23 +74,6 @@ impl RateLimit {
 			}
 		}
 	}
-}
-
-#[derive(Debug, Clone)]
-pub struct TradesParams {
-	pub symbol: String,
-	pub start_time: Timestamp,
-	pub end_time: Timestamp,
-	pub id: String,
-}
-impl TradesParams {
-	pub fn build(symbol: String, start_time: Timestamp, end_time: Timestamp, id: String) -> TradesParams {
-		assert!(start_time.ms < end_time.ms, "Panic during building the TradesParams: start_time must be less than end_time");
-		TradesParams { symbol, start_time, end_time, id }
-	}
-}
-pub enum Params {
-	TradesParams,
 }
 
 //=============================================================================
