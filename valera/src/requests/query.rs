050cc098e4be815783b62e0d86caffc8790751b2
Valera6 <v79166789533@gmail.com>
2023-11-22 21:21:41 +0000

    .

--- valera/src/requests/query.rs
+++ valera/src/requests/query.rs
@@ -1,4 +1,5 @@
-use anyhow::Result;
+use anyhow::Ruse anyhow::{Context, Result};esult;
+use polars::prelude::*;
 use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
 
@@ -62,7 +63,7 @@ impl<T> SubQuery<T> {
 		}
 	}
 	/// A wrapper function around `logic` field of the SubQuery, that 1) allows access to all its fields, which would normally be difficult to get the compiler to like, 2) convenient way to implement repetetivve things, like checking whether `start_time` and `end_time` are `Some` to determine whether the request is singular, or we should loop.
-	pub async fn execute(&self) {
+	pub async fn execute(&self, client: &Client) {
 		//TODO!: 1) put the logic into a closure, use `move` keywoard if needed.
 		//TODO!: 2) move out to be held by the `logic` field
 
@@ -70,7 +71,12 @@ impl<T> SubQuery<T> {
 		// also, this allows us to assume start_time and end_time are definitely provided.
 		let find_fromid = async {
 			let base_url = self.url.clone().rsplitn(2, '/').nth(1).unwrap_or_default();
-			let url = format!("{}/aggTrades?symbol={}&startTime={}&limit=1", base_url, self.other_params.get("symbol").unwrap(), self.start_time.clone().unwrap().ms);
+			let url = format!(
+				"{}/aggTrades?symbol={}&startTime={}&limit=1",
+				base_url,
+				self.other_params.get("symbol").unwrap(),
+				self.start_time.clone().unwrap().ms
+			);
 			let json = reqwest::get(&url).await.unwrap().json::<serde_json::Value>().await.unwrap();
 			json[0]["f"].as_i64().unwrap().to_string()
 		}
@@ -81,30 +87,30 @@ impl<T> SubQuery<T> {
 		let mut params = self.other_params;
 		params.insert("fromId".to_owned(), find_fromid);
 
-		//	let mut last_reached_ms = *start_time.get_ms();
-		//	let mut buffer_df = DataFrame::default();
-		//	while last_reached_ms < end_time.ms {
-		//		// In the perfect world check the code, and never print out the same error code twice.
-		//		let r = match client::request(url, params)?.await() {
-		//			Error(e) => eprintln!("Request errored: {}", e),
-		//			Result(response) => response,
-		//		};
-		//
-		//		let r_json = r.json::<serde_json::Value>().await.unwrap();
-		//
-		//		let array_of_values = r_json.as_array().unwrap().to_vec();
-		//		let mut new_fromId = String::new();
-		//		for v in array_of_values.iter() {
-		//			let t = v.get("time").unwrap().as_i64().unwrap();
-		//			if t <= end_time.ms {
-		//				let row: DataFrame = market.trades_entry_into_row(&v);
-		//				buffer_df.vstack_mut(&row)?;
-		//			}
-		//			new_fromId = (v.get("id").unwrap().as_i64().unwrap() + 1).to_string(); // because the thing is inclusive, I checked.
-		//			last_reached_ms = v.get("time").unwrap().as_i64().unwrap();
-		//		}
-		//		params.insert("fromId", Box::leak(new_fromId.clone().into_boxed_str()));
-		//	}
+		let mut last_reached_ms = self.start_time.unwrap().ms;
+		let mut buffer_df = DataFrame::default();
+		while last_reached_ms < self.end_time.unwrap().ms {
+			// In the perfect world check the code, and never print out the same error code twice.
+			let r = match client.request(url, &params).await {
+				Ok(response) => response,
+				Err(e) => eprintln!("Request errored: {}", e),
+			};
+
+			let r_json = r.json::<serde_json::Value>().await.unwrap();
+
+			let array_of_values = r_json.as_array().unwrap().to_vec();
+			let mut new_fromid = String::new();
+			for v in array_of_values.iter() {
+				let t = v.get("time").unwrap().as_i64().unwrap();
+				if t <= end_time.ms {
+					let row: DataFrame = market.trades_entry_into_row(&v);
+					buffer_df.vstack_mut(&row).unwrap();
+				}
+				new_from_id = (v.get("id").unwrap().as_i64().unwrap() + 1).to_string(); // because the thing is inclusive, I checked.
+				last_reached_ms = v.get("time").unwrap().as_i64().unwrap();
+			}
+			params.insert("fromId".to_owned(), Box::leak(new_from_id.clone().into_boxed_str()));
+		}
 		self.parent.append_result(buffer_df);
 	}
 }
